e1d2bee909a818d184373cac04b50bd8125ca2fc
Manfred Ariel Martinez Bastos <67986156+ManfredAriel@users.noreply.github.com>
2024-03-29 17:35:28 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -3,5 +3,5 @@ local variable1 = "Hola"
 local variable2 = 42
 
 -- Pruebas de inicialización
-assert(variable1 == "Hola", "Variable1 no se inicializó correctamente")
+assert(variable1 == "Hola2", "Variable1 no se inicializó correctamente")
 assert(variable2 == 42, "Variable2 no se inicializó correctamente")
